eregon/jvmci

Coroutines: rename CoroutineSupport to NativeCoroutineSupport

src/share/vm/classfile/vmSymbols.hpp
@@ -598,7 +598,7 @@
   template(classLoader_name,                           "classLoader")                                             \
                                                                                                                   \
   /* coroutine support */                                                                                         \
-  template(com_oracle_truffle_coro_CoroutineSupport,   "com/oracle/truffle/coro/CoroutineSupport")                \
+  template(com_oracle_truffle_coro_CoroutineSupport,   "com/oracle/truffle/coro/NativeCoroutineSupport")          \
   template(com_oracle_truffle_coro_CoroutineBase,      "com/oracle/truffle/coro/CoroutineBase")                   \
   template(com_oracle_truffle_coro_CoroutineExitException, "com/oracle/truffle/coro/CoroutineExitException")      \
   template(data_name,                                  "data")                                                    \

eregon/graal-jvmci-8

src/share/vm/classfile/vmSymbols.hpp
@@ -604,7 +604,7 @@
   template(classLoader_name,                           "classLoader")                                             \
                                                                                                                   \
   /* coroutine support */                                                                                         \
-  template(java_dyn_CoroutineSupport,                  "com/oracle/truffle/coro/CoroutineSupport")                \
+  template(java_dyn_CoroutineSupport,                  "com/oracle/truffle/coro/NativeCoroutineSupport")          \
   template(java_dyn_CoroutineBase,                     "com/oracle/truffle/coro/CoroutineBase")                   \
   template(java_dyn_CoroutineExitException,            "com/oracle/truffle/coro/CoroutineExitException")          \
   template(data_name,                                  "data")                                                    \